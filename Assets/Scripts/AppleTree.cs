dbb813c4280d24630e3d58b9e1b105f7856be2a6
AngelCj1 <70732152+AngelCj1@users.noreply.github.com>
2020-09-17 22:29:59 -0400

    Change Update
    Updated the script for changing direction

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -2,28 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree : MonoBehaviour {
+public class AppleTree : MonoBehaviour
+{
     [Header("Set in Inspector")]
     // Prefab for instantiating apples
-    public GameObject   applePrefab;
+    public GameObject applePrefab;
 
     // Speed at which the AppleTree moves
-    public float        speed = 1f;
-    
+    public float speed = 1f;
+
     // Distance where AppleTree turns around
-    public float        leftAndRightEdge = 10f;
-    
+    public float leftAndRightEdge = 10f;
+
     // Chance that the AppleTree will change directions
-    public float        chanceToChangeDirections = 0.1f;
-    
+    public float chanceToChangeDirections = 0.1f;
+
     // Rate at which Apples will be instantiated
-    public float        secondsBetweenAppleDrops= 1f;
-    
-    void Start () {
+    public float secondsBetweenAppleDrops = 1f;
+
+    void Start()
+    {
         // Dropping apples every second
     }
 
-    void Update () {
+    void Update()
+    {
         // Basic Movement
 
         Vector3 pos = transform.position;
@@ -35,6 +38,10 @@ public class AppleTree : MonoBehaviour {
 
 
         // Changing Direction
+        if ( pos.x < -leftAndRightEdge ) { 
+            speed = Mathf.Abs(speed); // Move right
+        } else if (pos.x > leftAndRightEdge) { 
+            speed = -Mathf.Abs(speed); // Move left
+        }
     }
-
 }
